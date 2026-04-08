aa25eba71b686b0696a4443bf73c489a5bfda6a7
francesco-troise <francesco.troise98@gmail.com>
2026-04-08 14:11:35 +0200

    Add info fields to the train table

--- database/migrations/2026_04_08_115807_create_trains_table.php
+++ database/migrations/2026_04_08_115807_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda', 50);
+            $table->string('Stazione_partenza', 50);
+            $table->string('Stazione_arrivo', 50);
+            $table->dateTime('Orario_partenza');
+            $table->dateTime('Orario_arrivo');
+            $table->tinyInteger('Totale_carrozze');
+            $table->tinyInteger('Ritardo');
+            $table->boolean('Treno_soppresso')->default(false);
+            $table->string('train_code', 15)->unique();
             $table->timestamps();
         });
     }
